5b366d43011ab62000ee4b9d42d5f183e716072f
vilfein <vilfein@gmail.com>
2023-05-05 21:57:50 +0200

    Add project files.

---
+++ MyDataSafe/Database/DBClass.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyDataSafe.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDataSafe.Database
+{
+    public class Context : DbContext
+    {
+        public DbSet<DataClass> Datas { get; set; }
+        public Context() : base()
+        { 
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Data Source = SafeDatas.db");
+        }
+    }
+}
